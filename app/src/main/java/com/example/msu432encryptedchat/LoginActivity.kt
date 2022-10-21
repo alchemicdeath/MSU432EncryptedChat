7ebe6a54ce4ef5a05a98b37e2c7caa4eefdfd266
jetfi <jordanpaigediedrich@gmail.com>
2022-10-21 07:37:34 -0500

    Initial commit

---
+++ app/src/main/java/com/example/msu432encryptedchat/LoginActivity.kt
@@ -0,0 +1,11 @@
+package com.example.msu432encryptedchat
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.login_activity)
+    }
+}
